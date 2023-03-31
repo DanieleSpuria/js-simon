e0b4e7243e79e91a70f706d35ca21d9acc611b30
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-31 15:36:45 +0200

    add input, button, h1

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/*
+- input, bottone, h1, due array vuoti;
+- numeri random unici e salvarli in un array;
+- timer di 5s;
+- far sparire i numeri;
+- salvare i numeri inseriti tramite input nell'altro array;
+- confrontare i due array e stampare i numeri uguali;
+*/
+
+
+
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const output = document.querySelector('h1');
+
+
