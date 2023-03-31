37ed3e9a22f1da8144d8eb787507346b0c15ee3b
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-31 15:38:29 +0200

    add nmberRandom, numberUser

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,5 @@ const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const output = document.querySelector('h1');
 
-
+let numberRandom = [];
+let numberUser = [];
